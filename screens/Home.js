e704a7578d9ace1cfec9a7ad10e573491c8015b0
Bill-linho <billlinho37@gmail.com>
2025-10-29 17:18:35 -0300

    4

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,41 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 export default function Home({navigation}) {
     return(
-        <View>
-            <Text>
+        <View style={style.container}>
+            <Text style={style.title}>
                 Bem vindo!
             </Text>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Recipes')}>
-                <Text>Ver Receitas</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Recipes')} style={style.button}>
+                <Text style={style.textButton}>Ver Receitas</Text>
             </TouchableOpacity>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 28
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      color:'#FF0000',
+      textAlign: 'center'
+    },
+    button:{
+        backgroundColor: '#000',
+        color: '#FFF',
+        padding: 15,
+        borderRadius: 10
+    },
+    textButton: {
+        color: '#FFF',
+        fontWeight: 'bold'
+    }
+  })
--- screens/Recipes.js
+++ screens/Recipes.js
@@ -1,15 +1,49 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+
+const dados_fake = [
+    {id: 1,nome: 'Bolo de banana', modo_preparo: 'Mistura', ingredientes: 'Banana, caramelo, trigo e ovo', usuario_id:1, categoria_id:1 } ,
+    {id: 2,nome: 'Bolo de morango', modo_preparo: 'Mistura', ingredientes: 'Morango, trigo e ovo', usuario_id:1, categoria_id:1}
+]
 
 export default function Receitas({navigation}) {
+const [view, setView] = useState('');
+
  return(
-        <View>
-            <Text>
+        <View style={style.container}>
+            <Text style={style.title}>
                 Receitas
             </Text>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-                <Text>Voltar</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={style.button}>
+                <Text style={style.textButton}>Voltar</Text>
             </TouchableOpacity>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 28
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      color:'#FF0000',
+      textAlign: 'center'
+    },
+    button:{
+        backgroundColor: '#000',
+        color: '#FFF',
+        padding: 15,
+        borderRadius: 10
+    },
+    textButton: {
+        color: '#FFF',
+        fontWeight: 'bold'
+    }
+  })
